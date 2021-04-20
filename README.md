b29b7e803cce0f290a4a5c416b98b6bf6c690667
Donboz <71496099+Donboz@users.noreply.github.com>
2021-04-20 01:18:20 +0000

    farbesquad-random

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# farbesquad.random
+# farbesquad-random
 
 `Rastegele gif ve resim atan modül `
 
@@ -6,37 +6,31 @@
 
 `npm install farbesquad.random --save`
 
-# Neden farbesquad.random
+# Neden farbesquad-random
 
-`Çok geniş kullanımlı ve aşırı detaylı bir modül olduğundan`
+`Geniş kullanımlı , giphy api, ve kararlı yapıda`
 
 # Methodlar
 
 `random`
-`otorandom`
 `search`
 
-# Komutlar v1.0.2
+# Komutlar v1.0.0
 
 ```
-const fbr = require("farbesquad.random")
+const fbr = require("farbesquad-random")
 
-let gif1 = fbr.random({tag:""})
 
+let gif1 = fbr.random()
 const e1 = new Discord.MessageEmbed()
 .setImage(gif1)
 
-let gif2 = fbr.otorandom()
+
+let gif2 = fbr.search({query:""})
 const e2 = new Discord.MessageEmbed()
 .setImage(gif2)
 
-
-let gif3 = fbr.search({query:""})
-const e3 = new Discord.MessageEmbed()
-.setImage(gif3)
-
 message.channel.send(e1)
 message.channel.send(e2)
-message.channel.send(e3)
 
 ```
--- index.js
+++ index.js
@@ -2,28 +2,13 @@ const axios = require("axios");
 const errmsg = "Herhangi bir sıkıntımı oldu / any problems ? discord: ! ! Donboz#7323 \n  "
 const api = "16GnF6kqQInS7QKMd8HPjcXZX2udEv40"
 
-/**
- * @param {object} options 
- * 
- */
-    async function random(options = {}) {
-        if(!options.tag) throw new Error("tag yeri boş kalamaz")
-         const a = await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}&tag=${options.tag}`)
-         const aa = await a.data.images.original.url
-         console.log(aa)
-         return await aa
-        } // FarbeSquad Studios
-
         /**
  * @param {object} options 
  */
-    async function otorandom() {
-        const a = await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}`)
-        const aa = await a.data.images.original.url
-        console.log(aa)
-        return await aa
+         async function random() {
+            return await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}`).then(async a => a.data.data.images.original.url)
         } // FarbeSquad Studios
-
+       
 
         /**
  * @param {object} options 
@@ -31,11 +16,8 @@ const api = "16GnF6kqQInS7QKMd8HPjcXZX2udEv40"
  */
     async function search(options = {}) {
         if(!options.query) throw new Error("arama yeri boş kalamaz")
-        const a = await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}&q=${options.query}`)
-        const aa = await a.data[0].images.original.url
-        console.log(aa)
-        return await aa
+        return await axios.get(`https://api.giphy.com/v1/gifs/search?&api_key=${api}&q=${options.query}`).then(async a => a.data.data[0].images.original.url)
     } // FarbeSquad Studios
    
 
-module.exports = {random,otorandom,search}
+module.exports = {random,search}
