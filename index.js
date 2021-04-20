12c7cb25f5e3b14f7678a5e648b024416404fe9c
Donboz <71496099+Donboz@users.noreply.github.com>
2021-04-20 01:11:07 +0000

    farbesquad-random

--- index.js
+++ index.js
@@ -4,34 +4,36 @@ const api = "16GnF6kqQInS7QKMd8HPjcXZX2udEv40"
 
 /**
  * @param {object} options 
- * @returns {Data}
+ * 
  */
     async function random(options = {}) {
         if(!options.tag) throw new Error("tag yeri boş kalamaz")
          const a = await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}&tag=${options.tag}`)
          const aa = await a.data.images.original.url
+         console.log(aa)
          return await aa
         } // FarbeSquad Studios
 
         /**
  * @param {object} options 
- * @returns {Data}
  */
     async function otorandom() {
-        const a = await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}}`)
+        const a = await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}`)
         const aa = await a.data.images.original.url
+        console.log(aa)
         return await aa
         } // FarbeSquad Studios
 
 
         /**
  * @param {object} options 
- * @returns {Data}
+ * 
  */
     async function search(options = {}) {
         if(!options.query) throw new Error("arama yeri boş kalamaz")
         const a = await axios.get(`https://api.giphy.com/v1/gifs/random?&api_key=${api}&q=${options.query}`)
-        const aa = await a.data.images.original.url
+        const aa = await a.data[0].images.original.url
+        console.log(aa)
         return await aa
     } // FarbeSquad Studios
    
